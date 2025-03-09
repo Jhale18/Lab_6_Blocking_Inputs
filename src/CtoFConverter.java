25e6b14d2e03a2e21ddc9da0d8e5b3efb0c9092a
Jhale18 <hale2jc@mail.uc.edu>
2025-03-09 15:59:44 -0400

    Update CtoFConverter.java

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,6 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class CtoFConverter {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double celsius = 0;
+        while (true) {
+            System.out.print("What's the temperature in Celsius? ");
+            if (input.hasNextDouble()) {
+                celsius = input.nextDouble();
+                break;
+            } else {
+                System.out.println("Please enter a number.");
+                input.next();
+            }
+        }
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.printf("That's %.2f°F.\n", fahrenheit);
     }
 }
+ 
