cc3d707d20a1ce9637eda53952bb0471f3d00517
Jhale18 <hale2jc@mail.uc.edu>
2025-03-09 15:58:51 -0400

    Update HighOrLow2.java

--- HighOrLow2/src/HighOrLow2.java
+++ HighOrLow2/src/HighOrLow2.java
@@ -1,8 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
+
 public class HighOrLow2 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
+        int randomNum = random.nextInt(10) + 1;
+        int guess = 0;
+        do {
+            System.out.print("Guess a number 1-10 ");
+            if (input.hasNextInt()) {
+                guess = input.nextInt();
+                if (guess < 1 || guess > 10) {
+                    System.out.println("Please enter a number between 1 and 10.");
+                }
+            } else {
+                System.out.println("error, please enter a valid number.");
+                input.next();
+            }
+        } while (guess < 1 || guess > 10);
+        System.out.println("The random number was: " + randomNum);
+        if (guess < randomNum) {
+            System.out.println("The guess is too low.");
+        } else if (guess > randomNum) {
+            System.out.println("The guess is too high.");
+        } else {
+            System.out.println("your guess was correct");
+        }
     }
 }
